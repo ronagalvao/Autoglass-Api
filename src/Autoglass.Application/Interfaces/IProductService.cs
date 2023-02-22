2a4be828958763793b6153dd5ea0e98daefdbf3c
Ronaldo Galvão <ronaldogalvao5150@gmail.com>
2023-02-22 14:42:06 -0300

    [?] - Alteraçao da assinatura do método que filtra produtos

--- src/Autoglass.Application/Interfaces/IProductService.cs
+++ src/Autoglass.Application/Interfaces/IProductService.cs
@@ -5,7 +5,7 @@ namespace Autoglass.Application.Interfaces
     public interface IProductService
     {
         Task<Product> GetProductByIdAsync(int id);
-        Task<IEnumerable<Product>> GetProductsAsync(int? vendorCode, DateTime? manufacturingDate, DateTime? expirationDate, int? pageNumber, int? pageSize);
+        Task<IEnumerable<Product>> GetProductsAsync(string? description, DateTime? manufacturingDate, DateTime? expirationDate);
         Task AddProductAsync(Product product);
         Task UpdateProductAsync(Product product);
         Task DeleteProductAsync(int id);
