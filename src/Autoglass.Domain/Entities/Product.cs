50341b3a207a80d23d068c02cd0b8d4db8998541
Ronaldo Galvão <ronaldogalvao5150@gmail.com>
2023-02-22 21:38:09 -0300

    [?] Ajuste no modelo de produtos e implementação do construtor

--- src/Autoglass.Domain/Entities/Product.cs
+++ src/Autoglass.Domain/Entities/Product.cs
@@ -3,13 +3,25 @@
 public class Product
 {
     public int Id { get; set; }
-    public string? Description { get; set; }
+    public string Description { get; set; }
     public ProductStatus Status { get; set; }
     public DateTime ManufacturingDate { get; set; }
     public DateTime ExpirationDate { get; set; }
     public int SupplierId { get; set; }
+    public string SupplierDescription { get; set; }
+    public string SupplierDocument { get; set; }
 
-    virtual public Supplier? Supplier { get; set; }
+    public Product(int id, string description, ProductStatus status, DateTime manufacturingDate, DateTime expirationDate, int supplierId, string supplierDescription, string supplierDocument)
+    {
+        Id = id;
+        Description = description;
+        Status = status;
+        ManufacturingDate = manufacturingDate;
+        ExpirationDate = expirationDate;
+        SupplierId = supplierId;
+        SupplierDescription = supplierDescription;
+        SupplierDocument = supplierDocument;
+    }
 }
 
 public enum ProductStatus
