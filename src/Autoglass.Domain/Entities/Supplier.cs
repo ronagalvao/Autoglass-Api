e431b1316fe319f1447c1a3621420e1318742bbe
Ronaldo Galvão <ronaldogalvao5150@gmail.com>
2023-02-22 21:38:29 -0300

    [?] - Ajuste no modelo do fornecedor

--- src/Autoglass.Domain/Entities/Supplier.cs
+++ src/Autoglass.Domain/Entities/Supplier.cs
@@ -5,7 +5,5 @@ namespace Autoglass.Domain.Entities
         public int Id { get; set; }
         public string? Description { get; set; }
         public string? CompanyDocument { get; set; }
-
-        public virtual ICollection<Product>? Products { get; set; }
     }
 }
