62a569290f612c7b574468f5c4cc54bce8adfbc9
Ronaldo Galvão <ronaldogalvao5150@gmail.com>
2023-02-24 13:50:19 -0300

    [?] - Inlusão de injeções de dependencias

--- src/Autoglass.API/Extensions/ServiceExtensions.cs
+++ src/Autoglass.API/Extensions/ServiceExtensions.cs
@@ -1,4 +1,8 @@
+using Autoglass.Application.Interfaces;
+using Autoglass.Application.Services;
+using Autoglass.Domain.Entities;
 using Autoglass.Domain.Interfaces.Repositories;
+using Autoglass.Domain.Validations;
 using Autoglass.Infrastructure.Repositories;
 
 namespace Autoglass.API.Extensions;
@@ -7,6 +11,9 @@ public static class ServiceExtensions
 {
     public static IServiceCollection AddServices(this IServiceCollection services) =>
         services
+            .AddScoped<IProductService, ProductService>()
+            .AddScoped<IUnitOfWork, UnitOfWork>()
+            .AddScoped<ProductValidation>()
             .AddScoped<IProductRepository, ProductRepository>()
             .AddScoped<ISupplierRepository, SupplierRepository>();
 }
