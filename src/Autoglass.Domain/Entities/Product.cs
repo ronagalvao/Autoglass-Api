6fc18335a4d300065f65bd8faead82c20d097d7a
Ronaldo Galvão <ronaldogalvao5150@gmail.com>
2023-03-02 17:48:42 -0300

    [?] - Criação de construtor vazio na classe de Produto

--- src/Autoglass.Domain/Entities/Product.cs
+++ src/Autoglass.Domain/Entities/Product.cs
@@ -2,8 +2,10 @@
 
 public class Product
 {
+    public Product() { }
+
     public int Id { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public ProductStatus Status
     {
         get => (ProductStatus)_status;
@@ -12,8 +14,8 @@ public class Product
     public DateTime ManufacturingDate { get; set; }
     public DateTime ExpirationDate { get; set; }
     public int SupplierId { get; set; }
-    public string SupplierDescription { get; set; }
-    public string SupplierDocument { get; set; }
+    public string? SupplierDescription { get; set; }
+    public string? SupplierDocument { get; set; }
 
     public Product(int id, string description, ProductStatus status, DateTime manufacturingDate, DateTime expirationDate, int supplierId, string supplierDescription, string supplierDocument)
     {
