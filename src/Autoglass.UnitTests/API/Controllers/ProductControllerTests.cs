243e371e2e7048065519cd84fe771adec175a091
Ronaldo Galvão <ronaldogalvao5150@gmail.com>
2023-03-03 10:11:58 -0300

    [?] - Ajustes nos unitTests de atualização de produtos

--- src/Autoglass.UnitTests/API/Controllers/ProductControllerTests.cs
+++ src/Autoglass.UnitTests/API/Controllers/ProductControllerTests.cs
@@ -18,7 +18,7 @@ public class ProductControllerTests
         private readonly Mock<IProductService> _productServiceMock;
         private readonly Mock<IMapper> _mapperMock;
         private readonly Mock<ProductValidation> _productValidationMock;
-        private readonly ProductsController _controller;
+        private readonly ProductsController _productController;
 
         public ProductsControllerTests()
         {
@@ -26,7 +26,7 @@ public class ProductControllerTests
             _mapperMock = new Mock<IMapper>();
             _productValidationMock = new Mock<ProductValidation>();
 
-            _controller = new ProductsController(
+            _productController = new ProductsController(
                 _productServiceMock.Object,
                 _mapperMock.Object,
                 _productValidationMock.Object
@@ -66,7 +66,7 @@ public class ProductControllerTests
                 .Returns(productDto);
 
             // Act
-            var result = await _controller.GetProductById(productId);
+            var result = await _productController.GetProductById(productId);
 
             // Assert
             Assert.IsType<OkObjectResult>(result);
@@ -82,7 +82,7 @@ public class ProductControllerTests
                 .ReturnsAsync((Product)null!);
 
             // Act
-            var result = await _controller.GetProductById(productId);
+            var result = await _productController.GetProductById(productId);
 
             // Assert
             Assert.IsType<NotFoundResult>(result);
@@ -96,8 +96,8 @@ public class ProductControllerTests
             var productId = 1;
             var productDescription = "Test Product";
             var status = ProductStatus.Active;
-            var manufacturingDate = new DateTime(2022, 01, 01);
-            var expirationDate = new DateTime(2022, 01, 01);
+            var manufacturingDate = new DateTime(2023, 01, 01);
+            var expirationDate = new DateTime(2024, 01, 01);
             var supplierId = 1;
             var supplierDescription = "Test Supplier";
             var supplierDocument = "1234567890";
@@ -145,7 +145,7 @@ public class ProductControllerTests
                 .Returns(productDtos);
 
             // Act
-            var result = await _controller.GetFilteredProduct(null, null, null, pageNumber, pageSize) as OkObjectResult;
+            var result = await _productController.GetFilteredProduct(null, null, null, pageNumber, pageSize) as OkObjectResult;
             var pagedList = result!.Value as PagedList<ProductDto>;
 
             // Assert
@@ -161,35 +161,35 @@ public class ProductControllerTests
         public async Task UpdateProduct_WithMatchingIdAndProductDto_ReturnsOkResult()
         {
             // Arrange
-            int id = 1;
-            var productDto = new ProductDto { Id = 1, Description = "Test product", ManufacturingDate = DateTime.Now, ExpirationDate = DateTime.Now.AddDays(30) };
-            
-            // Arrange
-            var productId = 1;
-            var productDescription = "Test Product";
-            var status = ProductStatus.Active;
-            var manufacturingDate = DateTime.Now;
-            var expirationDate = DateTime.Now.AddDays(30);
-            var supplierId = 1;
-            var supplierDescription = "Test Supplier";
-            var supplierDocument = "1234567890";
+            var productDto = new ProductDto
+            {
+                Id = 1,
+                Description = "Test product",
+                Status = ProductStatus.Active,
+                ManufacturingDate = DateTime.Now,
+                ExpirationDate = DateTime.Now.AddDays(30),
+                SupplierId = 1,
+                SupplierDescription = "Test Supplier",
+                SupplierDocument = "47.607.687/0001-91"
+            };
 
             var product = new Product
-            (
-                productId,
-                productDescription,
-                status,
-                manufacturingDate,
-                expirationDate,
-                supplierId,
-                supplierDescription,
-                supplierDocument
-            );
+            {
+                Id = 1,
+                Description = "Test product",
+                Status = ProductStatus.Active,
+                ManufacturingDate = DateTime.Now,
+                ExpirationDate = DateTime.Now.AddDays(30),
+                SupplierId = 1,
+                SupplierDescription = "Test Supplier",
+                SupplierDocument = "67.237.025/0001-84"
+            };
 
             _mapperMock.Setup(m => m.Map<Product>(productDto)).Returns(product);
-
+            _productServiceMock.Setup(s => s.CanBeUpdatedAsync(product.Id)).ReturnsAsync("");
+        
             // Act
-            var result = await _controller.UpdateProduct(id, productDto);
+            var result = await _productController.UpdateProduct(product.Id, productDto);
 
             // Assert
             result.Should().BeOfType<OkResult>();
@@ -200,14 +200,29 @@ public class ProductControllerTests
         public async Task UpdateProduct_WithNonMatchingIdAndProductDto_ReturnsBadRequest()
         {
             // Arrange
-            int id = 1;
-            var productDto = new ProductDto { Id = 2, Description = "Test product", ManufacturingDate = DateTime.Now, ExpirationDate = DateTime.Now.AddDays(30) };
-
+            int invalidId = 1;
+            var productDto = new ProductDto
+            {
+                Id = 2,
+                Description = "Test product",
+                Status = ProductStatus.Active,
+                ManufacturingDate = DateTime.Now,
+                ExpirationDate = DateTime.Now.AddDays(30),
+                SupplierId = 1,
+                SupplierDescription = "Test Supplier",
+                SupplierDocument = "47.607.687/0001-91"
+            };
+            
+            var product = new Product();
+            
             // Act
-            var result = await _controller.UpdateProduct(id, productDto);
+            _mapperMock.Setup(m => m.Map<Product>(productDto)).Returns(product);
+            _productServiceMock.Setup(s => s.CanBeUpdatedAsync(product.Id)).ReturnsAsync("");
 
+            var result = await _productController.UpdateProduct(invalidId, productDto);
+            
             // Assert
-            result.Should().BeOfType<BadRequestResult>();
+             result.Should().BeOfType<BadRequestObjectResult>();
             _productServiceMock.Verify(s => s.UpdateProductAsync(It.IsAny<Product>()), Times.Never);
         }
 
@@ -215,35 +230,38 @@ public class ProductControllerTests
         public async Task UpdateProduct_WithInvalidProductDto_ThrowsValidationException()
         {
             // Arrange
-            int id = 1;
-            var productDto = new ProductDto { Id = 1, Description = "Test product", ManufacturingDate = DateTime.Now, ExpirationDate = DateTime.Now.AddDays(30) };
-            
-            // Arrange
-            var productId = 1;
-            var productDescription = "Test Product";
-            var status = ProductStatus.Active;
-            var manufacturingDate = new DateTime(2022, 01, 01);
-            var expirationDate = new DateTime(2022, 01, 01);
-            var supplierId = 1;
-            var supplierDescription = "Test Supplier";
-            var supplierDocument = "1234567890";
+            var productDto = new ProductDto
+            {
+                Id = 1,
+                Description = "Test product",
+                Status = ProductStatus.Active,
+                ManufacturingDate = DateTime.Now,
+                ExpirationDate = DateTime.Now.AddDays(30),
+                SupplierId = 1,
+                SupplierDescription = "Test Supplier",
+                SupplierDocument = "67.237.025/0001-84"
+            };
 
             var product = new Product
-            (
-                productId,
-                productDescription,
-                status,
-                manufacturingDate,
-                expirationDate,
-                supplierId,
-                supplierDescription,
-                supplierDocument
-            );
+            {   
+                Id = 1,
+                Description = "Test product",
+                Status = ProductStatus.Active,
+                ManufacturingDate = new DateTime(2023, 03, 02),
+                ExpirationDate = new DateTime(2023, 03, 02),
+                SupplierId = 1,
+                SupplierDescription = "Test Supplier",
+                SupplierDocument = "67.237.025/0001-84"
+            };
 
             _mapperMock.Setup(m => m.Map<Product>(productDto)).Returns(product);
+            _productServiceMock.Setup(s => s.CanBeUpdatedAsync(product.Id)).ReturnsAsync("");
 
-            //Act & Assert
-            await Assert.ThrowsAsync<ValidationException>(() => _controller.UpdateProduct(id, productDto));
+            _productValidationMock.Setup(x => x.Validate(It.IsAny<ValidationContext<Product>>()))
+                .Throws(new ValidationException("Validation throws error"));
+
+            // Act && Assert
+            await Assert.ThrowsAsync<ValidationException>(() => _productController.UpdateProduct(product.Id, productDto));
         }
     }
 }
